c3a17f962c045ffea8e732f77f307c5b5a5827d6
Esther Aznar <eaznard@iesch.org>
2025-09-25 13:34:01 +0200

    dia 5

--- TEMA01/src/StreamBusquedas/Ejer05/Main.java
+++ TEMA01/src/StreamBusquedas/Ejer05/Main.java
@@ -2,8 +2,19 @@ package StreamBusquedas.Ejer05;
 
 import StreamBusquedas.Ejer05.modelo.Empleado;
 
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
+/*Crea la clase empleado con al menos los atributos: nombre, departamento.
+- Crea varios empleados y agrupar la lista de empleados por departamento.
+- Agrupamos los empleados por departamento y contamos cuantos empleados hay en
+cada departamento.
+- Dado un departamento mostraremos sus empleados. Por ejemplo, muestra los
+empleados de ventas.
+- Dado un nombre de empleado, mostraremos su departamento.*/
 public class Main {
 
     public static void main(String[] args) {
@@ -18,8 +29,38 @@ public class Main {
                 new Empleado("Jose", "Gestion"),
                 new Empleado("Andres", "Desarrollo"));
 
+        //ECHO POR MI
         //Agrupamos los empleados por departamento
+        Map<String, List<Empleado>> empleadosPorDep = empleados
+                .stream().collect(Collectors
+                        .groupingBy(Empleado::getDepartamento));
 
+        System.out.println("Los empleados agrupados por departamentos son: ");
+        empleadosPorDep.forEach((dep, lista) -> System.out.println(dep + ": " + lista));
+
+        //Contar empleados por departameto
+        Map<String, Long> contarDep = empleados.stream()
+                .collect(Collectors
+                        .groupingBy(Empleado::getDepartamento, Collectors.counting()));
+        System.out.println("\n Cantidad de empleados por departameto: ");
+        contarDep.forEach((dep, cantidad) -> System.out.println(dep + ": " + cantidad));
+
+        //Mostrar empleados de ventas
+        String depConsulta = "Ventaas";
+        System.out.println("\nEmpleados del departamento " + depConsulta + ":");
+        empleadosPorDep.getOrDefault(depConsulta, Collections.emptyList())
+                .forEach(emp -> System.out.println(emp.getNombre()));
+
+        //Dado un nombre muesta el departamento
+        String  nomConsulta = "Paco";
+        Optional<Empleado> buscar = empleados.stream()
+                .filter(e -> e.getNombre().equalsIgnoreCase(nomConsulta))
+                .findFirst();
+        if (buscar.isEmpty()){
+            System.out.println("\n" + nomConsulta + " trabaja en " + buscar.get().getDepartamento());
+        }else {
+            System.out.println("\nEmpleado no encontrado");
+        }
 
     }
 
