699249291e89b52cb793a502b75e8a168dbbac5b
unknown <eaznard@iesch.org>
2026-02-10 21:17:15 +0100

    sacacorchos

--- TEMA05/EjercicioDGT/src/main/java/org/iesch/EjercicioDGT/EjercicioDgtApplication.java
+++ TEMA05/EjercicioDGT/src/main/java/org/iesch/EjercicioDGT/EjercicioDgtApplication.java
@@ -3,6 +3,8 @@ package org.iesch.EjercicioDGT;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/*jjfahjskdfhafdka
+* sacacorchos*/
 @SpringBootApplication
 public class EjercicioDgtApplication {
 
